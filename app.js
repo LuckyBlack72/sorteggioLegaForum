60f29891c5a6c88e2fdfa5531d27e1dae102fc23
Luciano Fusco <luciano.fusco@nttdata.com>
2018-08-24 09:51:01 +0200

    Cross Domain Problem
    Cross Domain Problem

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req, res, next) {
   //Cross origin allowance
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Method", "POST, OPTIONS, GET");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
